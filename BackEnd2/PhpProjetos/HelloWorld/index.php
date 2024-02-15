f88844592eef4ffcb77985e44468ef7b8807db27
MuriloFiFer <murilo.fischer.senai23@outlook.com>
2024-02-14 21:38:03 -0300

    14/02/24 PHP BackEnd

---
+++ BackEnd2/PhpProjetos/HelloWorld/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Programação</title>
+<body>
+<h1>Olá Alunos!!! Vamos Aprender PHP????</h1>
+<?php
+echo "Oi, Eu serei visto na sua tela</br>";
+// Eu não! Sou apenas um comentário.
+echo "Oi, Eu também serei visto por você</br>";
+# Já eu não serei!
+echo "E eu aqui novamente na sua tela, rs</br>";
+/* Eu não aparecerei na sua tela novamente
+pois sou um comentário */
+?>
+</body>
+</html>
+</html>
