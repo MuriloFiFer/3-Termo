e19e0ef3672d86dc8cf4850185909a23b742db96
MuriloFiFer <murilo.fischer.senai23@outlook.com>
2024-02-21 19:35:13 -0300

    21/02/24

--- BackEnd2/PhpProjetos/ProjetoDoacaoPet/conectaBD.php
+++ BackEnd2/PhpProjetos/ProjetoDoacaoPet/conectaBD.php
@@ -5,19 +5,24 @@
 // senha
 $endereco = 'localhost';
 $banco = 'postgres';
-$usuario = 'postgres';
-$senha = 'postgres';
-try {
-// sgbd:host;port;dbname
-// usuario
-// senha
-// errmode
-$pdo = new PDO("pgsql:host=$endereco;port=5432;dbname=$banco", $usuario, $senha,
-[PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-echo "Conectado no banco de dados!!!";
+$adm = 'postgres';
+$senha = 'postgres7210';
 
-} catch (PDOException $e) {
-echo "Falha ao conectar ao banco de dados. <br/>";
-die($e->getMessage());
-}
-?>
+try {
+    // sgbd:host;port;dbname
+    // usuario
+    // senha
+    // errmode
+    $pdo = new PDO(
+        "pgsql:host=$endereco;port=5432;dbname=$banco",$adm,$senha,[PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+    );  
+    echo "Conectado no banco de dados!!!";
+
+    $sql = "CREATE TABLE IF NOT EXISTS usuario (ID serial, NOME varchar(255), DATA_NASCIMENTO varchar(255), TELEFONE varchar(255), EMAIL varchar(255), SENHA varchar(255))";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();
+
+} catch (PDOException $e){
+    echo "Falha ao conectar ao banco de dados.<br\>";
+        die ($e->getMessage());  
+    } 
