f501706ce05152bcc26564c3c42962c1695176ea
MuriloFiFer <murilo.fischer.senai23@outlook.com>
2024-03-25 21:50:10 -0300

    25/03/24

--- BancoDeDados/SQL/25_03_24_Exemplo1.sql
+++ BancoDeDados/SQL/25_03_24_Exemplo1.sql
@@ -9,13 +9,65 @@
 CREATE DATABASE DB_AULA25MAR24;
 
 CREATE TABLE Fornecedor(
-    Fcodigo INT NOT NULL,
+    Fcodigo INT NOT NULL,  --NOT NULL, VALOR NÃO PODE SER VAZIO
     Fnome VARCHAR(30) NOT NULL,
     Status INT,
     Cidade VARCHAR (30)
 );
 
 SELECT * FROM Fornecedor;  -- comando para selecionar a tabela e mostrar no "RESULTS"
+
+
+--cria a tabela se não existir 
+-- se existir nada acontece, inclusive erro
+CREATE TABLE IF NOT EXISTS Fornecedor(
+    Fcodigo INT NOT NULL,
+    Fnome VARCHAR(30) NOT NULL,
+    Status INT,
+    Cidade VARCHAR (30)
+);
+
+CREATE TABLE IF NOT EXISTS Departamento(
+    Dcodigo INT NOT NULL,
+    Dnome VARCHAR(30) NOT NULL,   
+    Cidade VARCHAR (30),
+    PRIMARY KEY (Dcodigo)
+);
+
+SELECT * FROM Departamento; 
+
+
+-- criação da tabela com Foreing Key (utiliza referencia de outra tabela)
+CREATE TABLE Empregado
+(
+    Ecodigo INT NOT NULL,   
+    Enome VARCHAR(40)NOT NULL,
+    CPF VARCHAR(15)NOT NULL,
+    Salario DECIMAL(7,2),
+    Dcodigo INT NOT NULL,
+    PRIMARY KEY(Ecodigo,Enome),
+    FOREIGN KEY(Dcodigo) REFERENCES Departamento
+)
+
+
+SELECT * FROM Empregado; 
+
+
+--Alterar Tabelas com o Alter Table
+--Adicionando coluna sexo
+ALTER TABLE Empregado ADD sexo CHAR(1);
+
+--Excluindo coluna sexo
+ALTER TABLE Empregado DROP COLUMN sexo;
+
+--Renomear a tabela
+ALTER TABLE Empregado RENAME TO funcionario;
+
+
+--Altera o tipo da tabela --ALTER COLLUM
+ALTER TABLE Fornecedor ALTER COLLUM status TYPE INT ativo TYPE BOOLEAN;
+
+
 
 
 
