5f4ef0d8980e14d45eb9bb0a646c34ec59c6b241
MuriloFiFer <murilo.fischer.senai23@outlook.com>
2024-04-03 21:45:26 -0300

    03/04/24

--- BackEnd2/CursoSpringBoot/escola_completo/src/main/java/webapp/escola_completo/Controller/IndexController.java
+++ BackEnd2/CursoSpringBoot/escola_completo/src/main/java/webapp/escola_completo/Controller/IndexController.java
@@ -4,29 +4,31 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
 @Controller
 public class IndexController {
-    //classe para criação das rotas de navegação
+    // Classe para criar rotas de navegação
 
+    // Rota para acessar a página inicial ("home")
     @GetMapping("/home")
     public String acessoHomePage() {
-        return "index";
+        return "index"; // Retorna a página index.html
     }
+
+    // Rota padrão, acessada quando não é especificada nenhuma outra rota
     @GetMapping("")
     public String acessoHomePage2() {
-        return "index";
+        return "index"; // Retorna a página index.html
     }
+
+    // Rota para acessar a página de login do administrador
     @GetMapping("/login-adm")
     public String acessoLoginAdm() {
-        return "login/login-adm";
+        return "login/login-adm"; // Retorna a página login-adm.html localizada na pasta login
     }
+
+    // Rota para acessar a página de cadastro de administrador
     @GetMapping("/cadastro-adm")
     public String acessoCadastroAdm() {
-        return "cadastro/cadastro-adm";
+        return "cadastro/cadastro-adm"; // Retorna a página cadastro-adm.html localizada na pasta cadastro
     }
-    
-    
-    
-    
 }
