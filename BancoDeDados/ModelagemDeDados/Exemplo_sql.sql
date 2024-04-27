ed996aec1d8f96e72879d8ec9deda3857afa368c
MuriloFiFer <murilo.fischer.senai23@outlook.com>
2024-04-26 21:43:48 -0300

    26/04/24 modelagem de dados

---
+++ BancoDeDados/ModelagemDeDados/Exemplo_sql.sql
@@ -0,0 +1,27 @@
+-- Geração de Modelo físico
+-- Sql ANSI 2003 - brModelo.
+
+
+
+CREATE TABLE Cliente (
+id_cliente serial PRIMARY KEY,
+celular_cliente varchar(15),
+nome_cliente varchar(50),
+cpf_cliente varchar(14)
+)
+
+CREATE TABLE Produto (
+valor_produto decimal(7,2),
+qtde_produto int,
+id_produto serial PRIMARY KEY
+)
+
+CREATE TABLE Compra (
+id_pedido serial PRIMARY KEY,
+data_compra_produto date,
+id_produto serial,
+id_cliente serial,
+FOREIGN KEY(id_produto) REFERENCES Produto (id_produto)
+FOREIGN KEY(id_cliente)REFERENCES Cliente (id_cliente)
+)
+
