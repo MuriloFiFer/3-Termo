f1669bd61943ec5ec496ffcc160d9704905de9ac
MuriloFiFer <murilo.fischer.senai23@outlook.com>
2024-04-28 21:37:42 -0300

    28/04/24 realizando primeiros teste, não conecta com banco

--- BackEnd2/CursoSpringBoot/SA_GerenciamentoEscolar/src/main/java/webapp/GerenciamentoEscolar/GerenciamentoEscola.java
+++ BackEnd2/CursoSpringBoot/SA_GerenciamentoEscolar/src/main/java/webapp/GerenciamentoEscolar/GerenciamentoEscola.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EscolaCompletoApplication {
+public class GerenciamentoEscola {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EscolaCompletoApplication.class, args);
+		SpringApplication.run(GerenciamentoEscola.class, args);
 	}
 
 }
