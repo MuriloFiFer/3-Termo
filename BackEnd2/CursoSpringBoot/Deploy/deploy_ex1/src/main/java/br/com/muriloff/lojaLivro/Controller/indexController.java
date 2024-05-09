62675a71add6c429b94830c4e750472cd2b7d856
MuriloFiFer <murilo.fischer.senai23@outlook.com>
2024-05-08 21:48:19 -0300

    08/05/24

---
+++ BackEnd2/CursoSpringBoot/Deploy/deploy_ex1/src/main/java/br/com/muriloff/lojaLivro/Controller/indexController.java
@@ -0,0 +1,14 @@
+package br.com.muriloff.lojaLivro.Controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.SpringFramework.stereotype.Controller;
+import org.SpringFramework.web.bind.annotation.GetMapping
+
+
+public interface LivroRepository extends CrudRepository<Livro.Integer>;{
+
+
+}
+
+    
