733402d16f4a63d20191af98c18df8cf5bc94160
MuriloFiFer <murilo.fischer.senai23@outlook.com>
2024-05-08 21:10:01 -0300

    08/05/24

---
+++ BackEnd2/CursoSpringBoot/Deploy/deploy_ex1/src/main/java/com/example/deploy_ex1/DeployEx1Application.java
@@ -0,0 +1,13 @@
+package com.example.deploy_ex1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployEx1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeployEx1Application.class, args);
+	}
+
+}
