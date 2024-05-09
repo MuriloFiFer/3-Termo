5ead96dc26658c53ace6404bb81d0906f563f2b0
MuriloFiFer <murilo.fischer.senai23@outlook.com>
2024-05-08 21:41:34 -0300

    08/05/24

--- BackEnd2/CursoSpringBoot/Deploy/deploy_ex1/src/main/java/br/com/muriloff/lojaLivro/Controller/LivrosController.java
+++ BackEnd2/CursoSpringBoot/Deploy/deploy_ex1/src/main/java/br/com/muriloff/lojaLivro/Controller/LivrosController.java
@@ -2,13 +2,30 @@ package br.com.muriloff.lojaLivro.Controller;
 
 import br.com.lojalivro.Repository.LivroRepository;
 
-public class LivrosController {
-    
-  
 
+@Controller
+public class LivrosController {  
+  
     
         @Autowired
         LivroRepository livroRepository;
+
+        @GetMapping("livros")
+        public ModelAndView getLivros() {
+            ModelAndView mv = new ModelAndView("livros");
+            mv.addObjct("livros", livroRepository.findAll());
+            return mv;
+        }
+
+        @GetMapping("/addlivro")
+        public String getAddLivro(){
+            return "livro-add";
+        }
     
+        @PostMapping("/addlivro")
+        public String postAddLivro(Livro livro){
+            livroRepository.save(livro);
+            return "redirect:/livros";
+        }
     }
 
