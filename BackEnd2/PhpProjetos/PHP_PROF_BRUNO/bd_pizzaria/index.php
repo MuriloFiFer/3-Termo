176c262699174370adc3037b2b6813d43c885167
MuriloFiFer <murilo.fischer.senai23@outlook.com>
2024-05-10 20:51:54 -0300

    10/05/24

---
+++ BackEnd2/PhpProjetos/PHP_PROF_BRUNO/bd_pizzaria/index.php
@@ -0,0 +1,12 @@
+<?php
+include 'functions.php';
+
+?>
+
+<?=template_header('Pizzaria Don Fernandes')?>
+
+<div class='content'>
+<h2>Inicio</h2>
+<p>Seja Bem Vindo"</p></div>
+
+<?=template_footer()?></div>
