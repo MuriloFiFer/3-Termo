486a3efc2a9aaeb39dc22a56a57cd7dd0a95d168
MuriloFiFer <murilo.fischer.senai23@outlook.com>
2024-05-29 19:50:59 -0300

    29/05/24 Inicio Projeto Controle de patrimonio SpringBoot

---
+++ BackEnd2/CursoSpringBoot/apirest_senai/src/main/java/com/example/apirest_senai/Model/Responsavel.java
@@ -0,0 +1,19 @@
+package com.example.apirest_senai.Model;
+
+
+import java.io.Serializable;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Setter
+@Getter
+public class Responsavel implements Serializable {
+    //atributos
+    @Id
+    private Long id;
+
+    private String nome;
+
+}
